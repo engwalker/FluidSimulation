60f87077a509bb9c62944c2a7b043ff33e7dbb76
Jeremy Engman <engwalker@gmail.com>
2022-09-13 21:44:14 -0700

    feat(testing): drew an openGL triangle

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 //	return 0;
 //}
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
@@ -17,6 +18,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
+    std::cout << "building hello world window";
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
     {
@@ -32,6 +34,11 @@ int main(void)
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
